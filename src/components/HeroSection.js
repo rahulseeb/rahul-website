559ed2dd94cce5351ae432e86b78b0c6362e1f02
rahulseeb <106937979+rahulseeb@users.noreply.github.com>
2023-08-10 16:38:40 -0400

    changing back the video

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -5,7 +5,7 @@ import './HeroSection.css';
 function HeroSection() {
   return (
     <div className="hero-container">
-        <video src='/videos/video-1.mp4' autoPlay loop muted />
+        <video src='/videos/video-2.mp4' autoPlay loop muted />
         <h1>Rahul Seebaransingh</h1>
         <p>A 3rd year Computer Science student at Queen's University</p>
     </div>

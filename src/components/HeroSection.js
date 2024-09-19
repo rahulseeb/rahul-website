c2642c6cc2be10c014cc21a82b00f5f6586802d5
rahulseeb <106937979+rahulseeb@users.noreply.github.com>
2024-09-19 16:01:34 -0400

    Update HeroSection.js

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,7 +7,7 @@ function HeroSection() {
     <div className="hero-container">
         <video src='/videos/molecules.mp4' autoPlay loop muted />
         <h1>Rahul Seebaransingh</h1>
-        <p>A 4th year Computer Science student at Queen's University</p>
+        <p>An upcoming graduate Computer Science student at Queen's University</p>
     </div>
   )
 }
